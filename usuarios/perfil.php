dda0741f7563d02b88abd2834de6bae111e69cda
zamukay <133037731+zamukay@users.noreply.github.com>
2025-05-27 17:00:09 -0500

    Agregacion de edicion de perfil del usuario

--- usuarios/perfil.php
+++ usuarios/perfil.php
@@ -79,7 +79,7 @@ if ($usuario = pg_fetch_assoc($result)) {
                 <h1 class="profile-name"><?= htmlspecialchars($nombreCompleto) ?></h1>
                 <p class="profile-title"><?= htmlspecialchars($carrera) ?></p>
             </div>
-            <a href="editar-perfil.php" class="profile-edit-btn">
+            <a href="editar_perfil.php" class="profile-edit-btn">
                 <i class="fas fa-user-edit"></i> Editar Perfil
             </a>
         </div>
