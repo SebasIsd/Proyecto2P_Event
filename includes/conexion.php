440971fd4ad0dbd936314724e928b0657acb8053
zamukay <133037731+zamukay@users.noreply.github.com>
2025-05-27 18:37:07 -0500

    Se actualizo la conexion

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,26 +1,26 @@
-    <?php
-    class Conexion {
-        private $conexion;
+<?php
+class Conexion {
+    private $conexion;
 
-        public function __construct() {
-            $host = "localhost";
-            $port = "5432";
-            $dbname = "gestion_cursos";
-            $user = "postgres";
-            $password = "ligacampeon";
+    public function __construct() {
+        $host = "mainline.proxy.rlwy.net";
+        $port = "48148";
+        $dbname = "railway";
+        $user = "postgres";
+        $password = "PlODJaMiNTNSbCvuomGjZfLVdPVzwQzY";
 
-            $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
-            $this->conexion = pg_connect($conn_string);
-        }
+        $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+        $this->conexion = pg_connect($conn_string);
+    }
 
-        public function getConexion() {
-            return $this->conexion;
-        }
+    public function getConexion() {
+        return $this->conexion;
+    }
 
-        public function cerrar() {
-            if ($this->conexion) {
-                pg_close($this->conexion);
-            }
+    public function cerrar() {
+        if ($this->conexion) {
+            pg_close($this->conexion);
         }
     }
-    ?>
+}
+?>
