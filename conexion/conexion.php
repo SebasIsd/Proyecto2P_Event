1f40b02536bd9c6a36c8ca07108a11cb565d8930
maribelsailema <vivianasarco123@gmail.com>
2025-05-26 23:41:14 -0500

    Se modifico la conexion para que no enviara mensajes de error ademas se implemento los archivos verEvento.js y verEvento.php al igual que colocar los estilos respectivos

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -5,15 +5,15 @@ class CConexion {
         $dbname = "railway"; // Reemplaza con el nombre de tu base de datos
         $username = "postgres"; // Reemplaza con tu usuario de base de datos
         $password = "PlODJaMiNTNSbCvuomGjZfLVdPVzwQzY"; // Reemplaza con tu contraseña de base de datos
-
+        $port = "48148";
         try {
-            $conn = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
-            echo "✅ Se conectó correctamente a la base";
+            $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
+            return $conn;
         } catch (PDOException $exp) {
-            echo "❌ No se puede conectar a la base: $exp";
+        return $null;
         }
 
-        return $conn;
+        
     }
 }
 
