7b8d2980098db68d380f511994f340edfc015ecb
SebasIsd <sdsnt2003@gmail.com>
2025-06-04 19:45:52 -0500

    cambios para la parte de inscripciones por tipo de evento

--- usuarios/inscripciones/get_eventos_disponibles.php
+++ usuarios/inscripciones/get_eventos_disponibles.php
@@ -23,7 +23,9 @@ try {
                 ec.ID_EVE_CUR as codigo,
                 ec.TIT_EVE_CUR as titulo,
                 ec.FEC_INI_EVE_CUR as fechaInicio,
-                ec.FEC_FIN_EVE_CUR as fechaFin
+                ec.FEC_FIN_EVE_CUR as fechaFin,
+                ec.COS_EVE_CUR as costo,
+                ec.TIP_EVE as tipo_evento
               FROM EVENTOS_CURSOS ec
               WHERE ec.FEC_FIN_EVE_CUR >= CURRENT_DATE";
     
