e20ff384802aa445d6dbd37142b32e14813fb90a
SebasIsd <sdsnt2003@gmail.com>
2025-07-07 03:09:22 -0500

    arreglar

--- includes/conexion1.php
+++ includes/conexion1.php
@@ -24,3 +24,35 @@ class Conexion {
     }
 }
 ?>
+
+<?php
+/*
+<?php
+class Conexion {
+    private $conexion;
+
+    public function __construct() {
+        // Leer desde variables de entorno (definidas en docker-compose.yml)
+        $host = getenv("DB_HOST");
+        $port = getenv("DB_PORT");
+        $dbname = getenv("DB_NAME");
+        $user = getenv("DB_USER");
+        $password = getenv("DB_PASSWORD");
+
+        $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+        $this->conexion = pg_connect($conn_string);
+    }
+
+    public function getConexion() {
+        return $this->conexion;
+    }
+
+    public function cerrar() {
+        if ($this->conexion) {
+            pg_close($this->conexion);
+        }
+    }
+}
+?>
+
+*/
