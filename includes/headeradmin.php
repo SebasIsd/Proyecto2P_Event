59c5ccbc5ef690b2abf9bec56974451c4d4f09e2
SebasIsd <sdsnt2003@gmail.com>
2025-07-07 12:40:41 -0500

    KAN-14 redirecciones de paginas

--- includes/headeradmin.php
+++ includes/headeradmin.php
@@ -29,7 +29,7 @@ if ($datos = pg_fetch_assoc($result)) {
     <ul>
       <li><a href="admin.php"><i class="fas fa-home"></i> Inicio</a></li>
       <li><a href="perfil.php"><i class="fas fa-user-circle"></i> Perfil</a></li>
-      <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Salir</a></li>
+      <li><a href="../usuarios/logout.php"><i class="fas fa-sign-out-alt"></i> Salir</a></li>
     </ul>
   </nav>
 </header>
